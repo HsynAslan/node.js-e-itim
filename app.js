72abd9a53b2f8c7c413cc73fa790b1d3885945ff
Hüseyin Aslan <394788@cs.ktu.edu.tr>
2023-10-20 14:01:16 +0300

    Post - Form

--- app.js
+++ app.js
@@ -14,6 +14,31 @@ function requestListener(request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });
     response.write("Anasayfa");
     response.end();
+  } else if (request.url == "/create" && request.method == "POST") {
+    const data = [];
+    request.on("data", (chunk) => {
+      data.push(chunk);
+    });
+    request.on("end", () => {
+      const result = Buffer.concat(data).toString();
+      const parsedData = result.split("=")[1];
+      console.log(parsedData);
+
+      fs.appendFile("blogs.txt", parsedData, (err) => {
+        if (err) console.log(err);
+        else {
+          response.statusCode = 302; // yönlendirme yapıcaz
+          response.setHeader("Location", "/");
+          response.end();
+        }
+      });
+    });
+  } else if (request.url == "/create") {
+    fs.readFile("pages/create.html", (error, html) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(html);
+      response.end();
+    });
   } else {
     fs.readFile("pages/404.html", (error, html) => {
       response.writeHead(404, { "Content-Type": "text/html" });
